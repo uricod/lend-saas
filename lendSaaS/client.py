fd134f4e2944b7852e600364278cafa53e41b000
uri@automationconslt.com <uri@automationconslt.com>
2022-03-23 09:02:23 -0400

    added version

--- lendSaaS/client.py
+++ lendSaaS/client.py
@@ -1,8 +1,15 @@
 from lendSaaS.src.leads import Leads
 from lendSaaS.src.transactions import Transactions
+from .__version__ import __version__
 
 
 class LendClient:
     def __init__(self, api_key, client_name):
         self.leads = Leads(api_key, client_name)
         self.transactions = Transactions(api_key, client_name)
+
+    def __str__(self):
+        return f'LendClient {__version__}'
+
+    def __repr__(self):
+        return f'LendClient {__version__}'
