beada1abe0ba79a69bb7f9fa964cc6dcab86bfa4
uri@automationconslt.com <uri@automationconslt.com>
2022-03-23 13:13:46 -0400

    move tests out to main folder

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,19 +1,13 @@
 import unittest
 from unittest.mock import patch
-from lendSaaS.client import LendClient
 import pandas as pd
 
 class BaseTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.lc = LendClient('api_key', 'client_name')
-
-    @patch('LendClient.make_request')
+    @patch('lendSaaS.client.LendClient.leads.make_request')
     def test_get_leads(self, mock_make_request):
         mock_make_request.return_value = pd.DataFrame({'no data': ['no data returned']})
         self.lc.get_leads()
         mock_make_request.assert_called_with('/lead', 'GET')
 
-    def tearDown(self):
-        pass
+
      
