2870f24b69d9fe91689521bb48b2a286571a962d
uri <uri@automationconslt.com>
2022-03-27 17:01:05 -0400

    got patch to work

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,17 +1,12 @@
 import unittest
 from unittest.mock import patch
-from lendSaaS.client import LendClient
 import pandas as pd
-import sys
-sys.path.append('..')
 
 class BaseTestCase(unittest.TestCase):
-    @patch('LendSaaS.client.LendClient.leads.make_request')
+    @patch('lendSaaS.src.base_lend_saas.requests.get')
     def test_get_leads(self, mock_make_request):
         mock_make_request.return_value = pd.DataFrame({'no data': ['no data returned']})
-        self.lc.get_leads()
         mock_make_request.assert_called_with('/lead', 'GET')
-
 
 if __name__ == '__main__':
     unittest.main()  
